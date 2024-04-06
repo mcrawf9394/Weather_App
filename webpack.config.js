0bd75b3bd89566ca87b42c769aa58d613e6c521a
mcrawf9394 <mjc9394@gmail.com>
2024-04-06 14:51:39 -0400

    I think that I finally finished setting up the webpack configurations and such

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,25 @@
 const path = require('path');
-
+const htmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+    plugins: [
+        new htmlWebpackPlugin({
+            template: './src/index.html',
+            filename: 'index.html',
+            inject: 'head',
+            scriptLoading: 'defer',
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
+    },
 };
